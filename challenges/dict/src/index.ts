462e406ea9caf06c6c05657a429bbb9bd98b9758
JustinSHong <jhong2009@gmail.com>
2019-12-08 20:02:13 -0800

    define Dict type with a T type param

--- challenges/dict/src/index.ts
+++ challenges/dict/src/index.ts
@@ -1,4 +1,6 @@
-export type Dict = {};
+export type Dict<T> = {
+    [key: string]: T | undefined; // a given key may not have a value
+};
 
 // Array.prototype.map, but for Dict
 export function mapDict() {}
